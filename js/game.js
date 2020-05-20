35172dcf88ad040ebbd96f6112627eca473cd305
Rotem10 <65648972+Rotem10@users.noreply.github.com>
2020-05-20 22:48:57 +0300

    end of day 1

--- js/game.js
+++ js/game.js
@@ -27,6 +27,7 @@ function buildBoard() {
     // Builds the board Set mines at random locations Call setMinesNegsCount() Return the created board
     var size = gLevel.size;
     var board = [];
+
     for (var i = 0; i < size; i++) {
         board.push([]);
         for (var j = 0; j < size; j++) {
@@ -35,22 +36,43 @@ function buildBoard() {
                 isShown: false,
                 isMine: false,
                 isMarked: false,
-                gameElement: ''
-            };
-            if (i === 1 && j === 0 || i === 2 && j === 2) {
-                board[i][j].isMine = true;
-                board[i][j].gameElement = MINE;
             };
         };
     };
+    placeMines(board);
     setMinesNegsCount(board);
     return board;
 };
 
+function placeMines(board) {
+    var minesPos = createMinesPos();
+    console.log(minesPos);
+    for (var i = 0; i < minesPos.length; i++) {
+        var currPos = minesPos[i];
+        board[currPos.i][currPos.j].isMine = true;
+    };
+    return board;
+};
+
+function createMinesPos() {
+    var mines = [];
+    for (var i = 0; i < gLevel.mines; i++) {
+        var randPos = getRandPos();
+        mines.push(randPos);
+    };
+    return mines;
+};
+
+function getRandPos() {
+    var randPos = {
+        i: getRandomIntInclusive(0, gLevel.size - 1),
+        j: getRandomIntInclusive(0, gLevel.size - 1)
+    };
+    return randPos;
+};
+
 function setMinesNegsCount(board) {
     // Count mines around each cell and set the cell's minesAroundCount.
-    // var minesCount = board[rowIdx][colIdx].minesAroundCount
-    // console.log(board[rowIdx][colIdx].minesAroundCount);
     for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < board[0].length; j++) {
             for (var rowIdx = i - 1; rowIdx <= i + 1; rowIdx++) {
@@ -58,8 +80,8 @@ function setMinesNegsCount(board) {
                 for (var colIdx = j - 1; colIdx <= j + 1; colIdx++) {
                     if (colIdx < 0 || colIdx > board[0].length - 1) continue;
                     if (rowIdx === i && colIdx === j) continue;
-                    var currCellContent = board[rowIdx][colIdx].gameElement;
-                    if (currCellContent === '*') board[i][j].minesAroundCount++;
+                    var currCellContent = board[rowIdx][colIdx].isMine;
+                    if (currCellContent) board[i][j].minesAroundCount++;
                 };
             };
         };
@@ -74,38 +96,10 @@ function renderBoard(board) {
         strHTML += `<tr>\n`;
         for (var j = 0; j < board[0].length; j++) {
             var tdId = [i, j];
-
             strHTML += `\t<td onclick="cellClicked(this,${i},${j})" class="cell" id="${tdId}" >\n \t</td>\n`;
         };
         strHTML += `</tr>\n`;
     };
-    // console.log('strHTML is:');
-    // console.log(strHTML);
     var elBoard = document.querySelector('.board');
     elBoard.innerHTML = strHTML;
-};
-
-function cellClicked(elCell, i, j) {
-    // Called when a cell (td) is clicked
-    var cell = gBoard[i][j];
-    // console.log(cell)
-    cell.isShown = true;
-    var minesCount = cell.minesAroundCount;
-    var presentedCount = '';
-    if (minesCount > 0) presentedCount = minesCount;
-    elCell.innerText = `${cell.gameElement} ${presentedCount}`;
-};
-
-function cellMarked(elCell) {
-    // Called on right click to mark a cell (suspected to be a mine) Search the web (and implement) how to hide the context menu on right click
-};
-
-function checkGameOver() {
-    // Game ends when all mines are marked, and all the other cells are shown
-};
-
-function expandShown(board, elCell, i, j) {
-    // When user clicks a cell with no mines around, we need to open not only that cell, but also its neighbors.
-    // NOTE: start with a basic implementation that only opens the non-mine 1st degree neighbors
-    // BONUS: if you have the time later, try to work more like the real algorithm (see description at the Bonuses section below)
 };
--- js/mineSweeper.js
+++ js/mineSweeper.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function cellClicked(elCell, i, j) {
+    // Called when a cell (td) is clicked
+    var cell = gBoard[i][j];
+    cell.isShown = true;
+    var minesCount = cell.minesAroundCount;
+    var presented = ''
+    if (!cell.isMine && minesCount > 0) {
+        var presented = minesCount;
+    } else if (cell.isMine) presented = MINE;
+
+    elCell.innerText = `${presented}`;
+};
+
+function cellMarked(elCell) {
+    // Called on right click to mark a cell (suspected to be a mine) Search the web (and implement) how to hide the context menu on right click
+};
+
+function checkGameOver() {
+    // Game ends when all mines are marked, and all the other cells are shown
+};
+
+function expandShown(board, elCell, i, j) {
+    // When user clicks a cell with no mines around, we need to open not only that cell, but also its neighbors.
+    // NOTE: start with a basic implementation that only opens the non-mine 1st degree neighbors
+    // BONUS: if you have the time later, try to work more like the real algorithm (see description at the Bonuses section below)
+};
